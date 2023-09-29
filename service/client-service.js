04a344709d604ec274964828e99a034c9651ce80
Melanie Brown <melaniebrown.bm@gmail.com>
2023-09-29 17:13:33 -0300

    retoques

--- service/client-service.js
+++ service/client-service.js
@@ -2,11 +2,11 @@
 const url = "";
 
 const listaClientes = () =>{
-    return fetch("https://.mockapi.io/user").then(respuesta =>{
+    return fetch("https://65170c27582f58d62d34be2a.mockapi.io/users").then(respuesta =>{
         return respuesta.json();
     });
 };
-const listaProductos = () => fetch("https://.mockapi.io/prod").then((respuesta) => respuesta.json()).catch((error) => error);
+const listaProductos = () => fetch("https://65170c27582f58d62d34be2a.mockapi.io/products").then((respuesta) => respuesta.json()).catch((error) => error);
 
 const crearCliente = (imagen, categoria, nombre_prod, precio_prod, descripcion_prod) => {
     console.log(imagen, categoria, nombre_prod, precio_prod, descripcion_prod)
@@ -26,18 +26,18 @@ const crearCliente = (imagen, categoria, nombre_prod, precio_prod, descripcion_p
 
 const eliminarCliente = (id) =>{
     console.log("eliminar a", id)
-    return fetch(`https://.mockapi.io/prod/${id}`, {
+    return fetch(`https://65170c27582f58d62d34be2a.mockapi.io/products${id}`, {
         method: "DELETE"
     })
 }
 
 const detalleProducto = (id) =>{
-    return fetch(`https://.mockapi.io/prod/${id}`)
+    return fetch(`https://65170c27582f58d62d34be2a.mockapi.io/products${id}`)
     .then( (respuesta) => respuesta.json())
 }
 
 const actualizarProducto = (imagen, categoria, nombre_prod, precio_prod, descripcion_prod,id) =>{
-    return fetch(`https://.mockapi.io/prod/${id}`,{
+    return fetch(`https://65170c27582f58d62d34be2a.mockapi.io/products${id}`,{
         method: "PUT",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({imagen, categoria, nombre_prod, precio_prod, descripcion_prod})
